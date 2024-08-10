ab40c640e4a2c0ebd3dabd517611a5dc87a1aea0
Panda-215 <151113211+Panda-215@users.noreply.github.com>
2024-08-10 15:53:03 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.css';
+import Navbar from './features/Navbar.js';
+import Container from './features/Container.js';
+import Home from './features/Home.js';
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Container>
+
+      <Home />
+      
+      </Container>
+
+    </>
+  );
+}
+
+export default App;
+
